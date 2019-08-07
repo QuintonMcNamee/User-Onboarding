5800bd3e971446d35bf9943ff3e9848a6a6a6e77
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-07 17:34:48 -0400

    step 2 complete (checkbox validation still WiP)

--- my-app/src/components/FormComponent.jsx
+++ my-app/src/components/FormComponent.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-const FormComponent = props => {
+const FormComponent = ({ errors, touched }) => {
   return (
     <Form>
-        <Field type="name" name="name" placeholder="Name" />
+        <Field type="text" name="name" placeholder="Name" />
+        {touched.name && errors.name && <div>{errors.name}</div>}
         <br />
         <Field type="email" name="email" placeholder="Email" />
+        {touched.email && errors.email && <div>{errors.email}</div>}
         <br />
         <Field type="password" name="password" placeholder="Password" />
+        {touched.password && errors.password && <div>{errors.password}</div>}
         <br />
         <Field component="select" name="favoriteColor">
           <option value="red">Red</option>
@@ -17,6 +21,7 @@ const FormComponent = props => {
         </Field>
         <br />
         Terms: <Field type="checkbox" name="tos" />
+        {touched.tos && errors.tos && <p>{errors.tos}</p>}
         <br />
         <button type="submit">Submit!</button>
     </Form>
@@ -24,12 +29,22 @@ const FormComponent = props => {
 };
 
 const FormikFormComponent = withFormik({
-  mapsPropsToValues({ email, password }) {
+  mapsPropsToValues({ name, email, password, favoriteColor, tos }) {
     return {
+      name: name || "",
       email: email || "",
-      password: password || ""
+      password: password || "",
+      favoriteColor: favoriteColor || "green",
+      tos: tos || false
     };
   },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string("Must be a string.").required("Name is required."),
+    email: Yup.string("Must be a string.").required("Email is required.").email("Not a valid email."),
+    password: Yup.string("Must be a string.").required("Password is required.").min(6, "Password must be atleast 6 characters."),
+    tos: Yup.bool().oneOf([true], "Terms are required.")
+  }),
 
   handleSubmit(values) {
     console.log('form submit', values);
