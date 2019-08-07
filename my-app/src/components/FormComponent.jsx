c58729016cb15900e003b5dba6ff2588fc35ed57
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-07 18:02:28 -0400

    part 4 completed

--- my-app/src/components/FormComponent.jsx
+++ my-app/src/components/FormComponent.jsx
@@ -11,30 +11,37 @@ const FormComponent = ({ errors, touched, values, handleSubmit, status }) => {
     if (status) {
       setUser([...user, status]);
     }
-  }, [status, user]);
+  }, [status]);
 
   return (
-    <Form>
-      <Field type="text" name="name" placeholder="Name" />
-      {touched.name && errors.name && <div>{errors.name}</div>}
-      <br />
-      <Field type="email" name="email" placeholder="Email" />
-      {touched.email && errors.email && <div>{errors.email}</div>}
-      <br />
-      <Field type="password" name="password" placeholder="Password" />
-      {touched.password && errors.password && <div>{errors.password}</div>}
-      <br />
-      <Field component="select" name="favoriteColor">
-        <option value="red">Red</option>
-        <option value="green">Green</option>
-        <option value="blue">Blue</option>
-      </Field>
-      <br />
-      Terms: <Field type="checkbox" name="tos" checked={values.tos} />
-      {touched.tos && errors.tos && <p>{errors.tos}</p>}
-      <br />
-      <button type="submit">Submit!</button>
-    </Form>
+    <div>
+      <Form>
+        <Field type="text" name="name" placeholder="Name" />
+        {touched.name && errors.name && <div>{errors.name}</div>}
+        <br />
+        <Field type="email" name="email" placeholder="Email" />
+        {touched.email && errors.email && <div>{errors.email}</div>}
+        <br />
+        <Field type="password" name="password" placeholder="Password" />
+        {touched.password && errors.password && <div>{errors.password}</div>}
+        <br />
+        <Field component="select" name="favoriteColor">
+          <option value="red">Red</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+        </Field>
+        <br />
+        Terms: <Field type="checkbox" name="tos" checked={values.tos} />
+        {touched.tos && errors.tos && <p>{errors.tos}</p>}
+        <br />
+        <button type="submit">Submit!</button>
+      </Form>
+      <div>
+        {user.map(user => (
+          <p key={user.id}>{user.createdAt}</p>
+        ))}
+      </div>
+    </div>
   );
 };
 
@@ -56,12 +63,12 @@ const FormikFormComponent = withFormik({
     tos: Yup.bool().oneOf([true], "Terms are required.")
   }),
 
-  handleSubmit(values, {setStatus}) {
+  handleSubmit(values, { setStatus }) {
     console.log('form submit', values);
     axios
-      .post('https://reqres.in/api/users')
+      .post('https://reqres.in/api/users', values)
       .then(res => {
-        console.log(res);
+        console.log('form received', res.data);
         setStatus(res.data);
       })
       .catch(err => console.log(err.response));
