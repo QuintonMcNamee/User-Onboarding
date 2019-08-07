ca188347ba36fb069a0fc2cd85fe821c2c2fced9
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-07 17:48:21 -0400

    step 3 complete (I think)

--- my-app/src/components/FormComponent.jsx
+++ my-app/src/components/FormComponent.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-const FormComponent = ({ errors, touched }) => {
+const FormComponent = ({ errors, touched, values, handleSubmit, status }) => {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    if (status) {
+      setUser([...user, status]);
+    }
+  }, [status, user]);
+
   return (
     <Form>
-        <Field type="text" name="name" placeholder="Name" />
-        {touched.name && errors.name && <div>{errors.name}</div>}
-        <br />
-        <Field type="email" name="email" placeholder="Email" />
-        {touched.email && errors.email && <div>{errors.email}</div>}
-        <br />
-        <Field type="password" name="password" placeholder="Password" />
-        {touched.password && errors.password && <div>{errors.password}</div>}
-        <br />
-        <Field component="select" name="favoriteColor">
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="blue">Blue</option>
-        </Field>
-        <br />
-        Terms: <Field type="checkbox" name="tos" />
-        {touched.tos && errors.tos && <p>{errors.tos}</p>}
-        <br />
-        <button type="submit">Submit!</button>
+      <Field type="text" name="name" placeholder="Name" />
+      {touched.name && errors.name && <div>{errors.name}</div>}
+      <br />
+      <Field type="email" name="email" placeholder="Email" />
+      {touched.email && errors.email && <div>{errors.email}</div>}
+      <br />
+      <Field type="password" name="password" placeholder="Password" />
+      {touched.password && errors.password && <div>{errors.password}</div>}
+      <br />
+      <Field component="select" name="favoriteColor">
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+        <option value="blue">Blue</option>
+      </Field>
+      <br />
+      Terms: <Field type="checkbox" name="tos" checked={values.tos} />
+      {touched.tos && errors.tos && <p>{errors.tos}</p>}
+      <br />
+      <button type="submit">Submit!</button>
     </Form>
   );
 };
@@ -46,8 +56,15 @@ const FormikFormComponent = withFormik({
     tos: Yup.bool().oneOf([true], "Terms are required.")
   }),
 
-  handleSubmit(values) {
+  handleSubmit(values, {setStatus}) {
     console.log('form submit', values);
+    axios
+      .post('https://reqres.in/api/users')
+      .then(res => {
+        console.log(res);
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.response));
   }
 })(FormComponent);
 
